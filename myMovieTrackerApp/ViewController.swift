7bead1144a180a626b879a4c93cd8ffe6d7feda3
Atakan Gedik <atakangdk210@msn.com>
2022-06-23 18:41:35 +0300

    Initial Commit

---
+++ myMovieTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myMovieTrackerApp
+//
+//  Created by Atakan Gedik on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
